29a36b96e8720a70509247160da4e984375184bc
Carlos Barreto <carlosmba96@gmail.com>
2022-03-06 16:06:12 -0400

    Project finished

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,30 +1,80 @@
 const container = document.querySelector("#container");
-const button = document.querySelector("#reload");
+const btnRainbow = document.querySelector(".rainbow");
+const btnBlack = document.querySelector(".black");
+const btnClear = document.querySelector(".clear");
+const btnEraser = document.querySelector(".eraser");
+const btnColorMode = document.querySelector(".color-mode");
+const slider = document.querySelector(".slider");
 
 for (let i = 0; i < 256; i++) {
-  div = document.createElement("div");
+  const div = document.createElement("div");
   container.appendChild(div);
-
-  div.addEventListener("mouseover", (e) => {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    e.target.style.backgroundColor = "#" + randomColor;
-  });
 }
 
-button.addEventListener("click", () => {
+let isMouseDown = false;
+
+btnRainbow.addEventListener("click", () => {
+  container.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+  container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+  container.addEventListener("mouseover", (e) => {
+    if (isMouseDown) {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      e.target.style.backgroundColor = "#" + randomColor;
+    }
+  });
+});
+
+btnBlack.addEventListener("click", () => {
+  container.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+  container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+  container.addEventListener("mouseover", (e) => {
+    if (isMouseDown) {
+      e.target.style.backgroundColor = "black";
+    }
+  });
+});
+
+btnEraser.addEventListener("click", () => {
+  container.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+  container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+  container.addEventListener("mouseover", (e) => {
+    if (isMouseDown) {
+      e.target.style.backgroundColor = "white";
+    }
+  });
+});
+
+btnClear.addEventListener("click", () => {
+  let divs = Array.from(document.querySelector("#container").children).forEach(
+    (element) => {
+      element.style.backgroundColor = "white";
+    }
+  );
+});
+
+function eventSlider() {
   document.querySelector("#container").textContent = "";
+  let valueSlider = document.querySelector(".slider").value;
+  let sizeGrid = Math.pow(valueSlider, 2);
 
-  const size = prompt("what size grid do you want?");
-  const sizeGrid = Math.pow(size, 2);
+  for (let i = 0; i < sizeGrid; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
 
-  if (sizeGrid >= 10000) {
-    alert("grid size cannot be greater than 100x100, press reset again");
-    return;
-  }
-
-  const eventSizeGrid = (size) => {
-    const columns = "1fr ".repeat(size);
-    const row = "1fr ".repeat(size);
+    const columns = "1fr ".repeat(valueSlider);
+    const row = "1fr ".repeat(valueSlider);
 
     container.setAttribute(
       "style",
@@ -33,16 +83,36 @@ button.addEventListener("click", () => {
       grid-template-rows: ${row};
       `
     );
-  };
-  eventSizeGrid(size);
-
-  for (let i = 0; i < sizeGrid; i++) {
-    div = document.createElement("div");
-    container.appendChild(div);
-
-    div.addEventListener("mouseover", (e) => {
-      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      e.target.style.backgroundColor = "#" + randomColor;
-    });
   }
+}
+
+// Create a new color picker instance
+// https://iro.js.org/guide.html#getting-started
+var colorPicker = new iro.ColorPicker(".colorPicker", {
+  // color picker options
+  // Option guide: https://iro.js.org/guide.html#color-picker-options
+  width: 280,
+  color: "rgb(255, 0, 0)",
+  borderWidth: 1,
+  borderColor: "#fff",
+});
+
+var values = document.getElementById("values");
+var hexInput = document.getElementById("hexInput");
+
+// https://iro.js.org/guide.html#color-picker-events
+colorPicker.on(["color:init", "color:change"], function (color) {
+  // Show the current color in different formats
+  // Using the selected color: https://iro.js.org/guide.html#selected-color-api
+  values.innerHTML = [
+    "hex: " + color.hexString,
+    "rgb: " + color.rgbString,
+    "hsl: " + color.hslString,
+  ].join("<br>");
+
+  hexInput.value = color.hexString;
+});
+
+hexInput.addEventListener("change", function () {
+  colorPicker.color.hexString = this.value;
 });
